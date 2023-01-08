ffa3a3ec3592046116ea88323cadaec91473c4f3
Milton Medina C <miltonmce@gmail.com>
2023-01-07 20:45:21 -0700

    add views to work and general settings

---
+++ services/frontend/pointofsales/src/views/HomeView.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="main-content">
+  <aside style="background-color:aqua ;">
+    gsdg
+  </aside>
+  <main style="background-color: brown;">
+    fdgdf
+  </main>
+</div>
+</template>
+<script>
+
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'HomeView',
+  computed : {
+    isLoggedIn: function() {
+      return this.$store.getters.isAuthenticated;
+    }
+  },
+});
+</script>
+
+<style>
+.main-content{
+  display: flex;
+}
+</style>
